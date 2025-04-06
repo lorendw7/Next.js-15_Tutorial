d4012dda6a27792557c3581f026d400b272915e3
lorendw7 <122377808+lorendw7@users.noreply.github.com>
2025-04-06 11:53:23 +0800

    Update page.tsx

--- authentication-demo/src/app/dashboard/page.tsx
+++ authentication-demo/src/app/dashboard/page.tsx
@@ -1,9 +1,6 @@
-import { auth, currentUser } from "@clerk/nextjs/server"
 
 
 export default async function DashBoardPage() {
-    const authObj = await auth();
-    const useObj = await currentUser();
     
     return <h1>DashBoard</h1>
 }
